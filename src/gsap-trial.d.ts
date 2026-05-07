62d8fa306b80a5134ae840e50b0058e25f144afb
Deepak-k21 <deepakkannika2@gmail.com>
2026-05-07 23:21:34 +0530

    Fix GSAP trial type errors for Vercel build

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,19 @@
+declare module 'gsap-trial/SplitText' {
+  export const SplitText: any;
+  export default SplitText;
+}
+
+declare module 'gsap-trial/ScrollSmoother' {
+  export const ScrollSmoother: any;
+  export default ScrollSmoother;
+}
+
+declare module 'gsap-trial/DrawSVGPlugin' {
+  export const DrawSVGPlugin: any;
+  export default DrawSVGPlugin;
+}
+
+declare module 'gsap-trial/ScrollToPlugin' {
+  export const ScrollToPlugin: any;
+  export default ScrollToPlugin;
+}
